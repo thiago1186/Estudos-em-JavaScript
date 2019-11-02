458ad2024cb21428e6d8a68fc200248bd4d2931a
thiago1186 <thiago.1186@gmail.com>
2019-11-02 14:48:59 -0300

    Add files via upload

---
+++ Estrutura de dados/stack.js
@@ -0,0 +1,39 @@
+class Stack{
+  constructor(arr = []){
+    this.itens = arr;
+  }
+
+  push(element){
+    this.itens[this.itens.length] = element;
+  }
+
+  pop(){
+    let value;
+    if(this.isEmpty()){
+      value = this.itens[this.itens.length - 1];
+      this.itens.length--;
+    }
+
+    return value;
+  }
+
+  peek(){
+    let value;
+    if(this.isEmpty()){
+      value = this.itens[this.itens.length - 1];
+    }
+    return value;
+  }
+
+  isEmpty(){
+    return this.itens.length == 0;
+  }
+
+  clear(){
+    this.itens = [];
+  }
+
+  size(){
+    return this.itens.length;
+  }
+}
